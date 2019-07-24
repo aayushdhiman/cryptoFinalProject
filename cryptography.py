b9a4268fe2d871b0b610e01d3b9743a7bc71137b
Aayush Dhiman <aayushdhiman@gmail.com>
2019-07-24 12:08:48 -0400

    Update cryptography.py
    cleaned up code -garbage collector

--- cryptography.py
+++ cryptography.py
@@ -3,7 +3,9 @@ from collections import Counter
 cipherText_dic = {"Affine": 1, "Multiplicative": 2, "Additive": 3, "Hill": 4, "Vigenere": 5,
                   "Rivest-Shamir-Alderman": 6}
 
+# Reference functions:
 
+# Calculates IC of text, returns decimal value
 def calc_ic(text):
     counts = Counter(text)
     num = 0.0
@@ -12,31 +14,77 @@ def calc_ic(text):
         i = val
         num += i * (i - 1)
         den += i
-    if (den == 0.0):
+    if den == 0.0:
         return 0.0
     else:
         return num / (den * (den - 1))
 
-
+# Encrypts letter A = 1
 def enc_letter(letter):
     return ord(letter.lower()) - 96
 
-
+# Decrypts letter A = 1
 def dec_letter(letter):
     if letter == 0:
         letter = 26
     return chr(letter + 96)
 
-
-def get_every_nth(text, key_length):
-    for i in range(key_length):
-        print(get_frequency(text[i::key_length]))
-
-
+# Frequency Analysis
 def get_frequency(text):
     return Counter(text)
 
 
+
+# def cipher_text():
+#     switcher = {
+#         1: "Affine",
+#         2: "Multiplicative",
+#         3: "Additive",
+#         4: "Vigenere",
+#         5: "Hill",
+#         6: "Rivest-Shamir-Alderman"
+#     }
+
+
+def add(num1, num2, mod=26):
+    return (num1 + num2) % mod
+
+
+def multiply(num1, num2, mod=26):
+    return (num1 * num2) % mod
+
+
+def encrypt_with_additive(plain_text, n, block_size):
+    cipher_text = ''
+    for index in range(len(plain_text)):
+        letter = enc_letter(plain_text[index].lower())
+        cipher_text += dec_letter(add(letter, n)).upper()
+        if index != 0 and (index - 1) % block_size == 0:
+            cipher_text += " "
+    return cipher_text
+
+
+def encrypt_with_multiplicative(plain_text, n, block_size):
+    cipher_text = ''
+    for index in range(len(plain_text)):
+        letter = enc_letter(plain_text[index].lower())
+        cipher_text += dec_letter(multiply(letter, n)).upper()
+        if index != 0 and (index - 1) % block_size == 0:
+            cipher_text += " "
+    return cipher_text
+
+
+def encrypt_with_affine(plain_text, n, r, block_size):
+    plain_text = encrypt_with_additive(plain_text, n, block_size)
+    plain_text = encrypt_with_multiplicative(plain_text.replace(" ", ''), r, block_size)
+    return plain_text
+
+# Finds every nth letter in a text when given the keyword length (Viginere)
+def get_every_nth(text, key_length):
+    for i in range(key_length):
+        print(get_frequency(text[i::key_length]))
+
+# Finds repeated strings in a text (Viginere)
 def find_repeats(text):
     repeats = []
     for length in range(2, 20):
@@ -48,62 +96,13 @@ def find_repeats(text):
     return repeats
 
 
-def determine_IC_type(ic):
+# Viginere input and decryption -> incomplete
+# message_input = input("Enter message: ")
+#
+# message_input = message_input.replace(" ", "").strip()
+# print(get_every_nth(message_input, 8))
+# print(find_repeats(message_input))
 
-
-message_input = input("Enter message: ")
-# message_input = "JNFEP DNBLZ BCDLL RYVNN MZCNA GLIGN EYNAR NMZRW HRRAA EENGG ANYAA CHUEG MISOM RGZZA AMTVF AFETP " \
-# "DYAGA SJRZY BXZEE ESQTH AHRQI AGKFA ARANL LMOEM SFSXY TYNSK OZNVH YEQRQ RCKLA ZCCCP XKWVC VJHBW " \
-# "PRMQE ETTWV UZOEI YESNB GEWAO GQSTU XZTFJ HGHXL NQXTG HXTEN UHUEX SECQO ATLDO ACDNY XLER"
-
-# message_input = "DDESOEOSDDHOQQHKMDHXJBBSQDTRYNBMHOPHGJEUHOQQOTDDGWHOQQWDNZYIJGHXWBBIEFAUOFVMWOKYANKCWBDDOFESCCDDGEGZYQXY"
-
-message_input = message_input.replace(" ", "").strip()
-print(get_every_nth(message_input, 8))
-print(find_repeats(message_input))
-
-
-def cipherText(cipherText):
-    Switcher = {
-        1: "Affine"
-        2: "Multiplicative"
-        3: "Additive"
-        4: "Vigenere"
-        5: "Hill"
-        6: "Rivest-Shamir-Alderman"
-    }
-
-    
-def add(num1, num2, mod=26):
-  return ((num1+num2) % mod)
-
-def mult (num1, num2, mod=26):
-  return ((num1*num2) % mod)
-
-def encrypt_with_additive(plain_text, n, block_size):
-    cipher_text = ''
-    for index in range(len(plain_text)):
-        letter = enc_letter(plain_text[index].lower())
-        cipher_text += dec_letter(add(letter, n)).upper()
-        if index!=0 and (index-1)%block_size==0:
-            cipher_text+=" "
-    return cipher_text
-
-def encrypt_with_multiplicative(plain_text, n, block_size):
-    cipher_text = ''
-    for index in range(len(plain_text)):
-        letter = enc_letter(plain_text[index].lower())
-        cipher_text += dec_letter(mult(letter, n)).upper()
-        if index!=0 and (index-1)%block_size==0:
-            cipher_text+=" "
-    return cipher_text
-  
-def encrypt_with_affine(plain_text, n, r, block_size):
-    plain_text = encrypt_with_additive(plain_text, n, block_size)
-    plain_text = encrypt_with_multiplicative(plain_text.replace(" ", ''), r, block_size)
-    return plain_text
-  
-  
 def encrypt_with_viginere(message, key):
     key_length = len(key)
     int_key = [ord(i) for i in key]
@@ -115,11 +114,3 @@ def encrypt_with_viginere(message, key):
         cipher_text += chr(value + 65)
 
     return cipher_text
-          
-    
-def sum(num1, num2, mod=26):
-    return ((num1 + num2) % mod)
-
-
-def mult(num1, num2, mod=26):
-    return ((num1 * num2) % mod)
