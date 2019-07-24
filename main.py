747626ff9e1b564a155f79e49e10c495e0ba83ec
Knakrack3 <42646291+Knakrack3@users.noreply.github.com>
2019-07-24 11:31:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ def mult (num1, num2, mod=26):
 def power (num1, num2, mod=26):
 
 def phi (num):
-  result = 1
-  for i in range (2, n):
-    if(gcd(i,n) == 1):
-      result+=1
+  list2 = factor(num, list1)
+  result = (list2[0]-1) * (list2[1]-1)
   return result
 
 def gcd(num1, num2):
@@ -19,5 +17,9 @@ def gcd(num1, num2):
     return(num2)
   return(gcd(num2 % num1, num1))
  
-def factor(num1):
- 
+def factor(num1, list1):
+ for i in range (2, int(num1 ** 0.5))
+   if(gcd(i,num1) !== 1):
+      list1.append(i)
+      factor((num1 / i), list1)    
+ return list1
