6732863bbf8e8569496f1b5c3769228da8f5d413
Knakrack3 <42646291+Knakrack3@users.noreply.github.com>
2019-07-24 10:55:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+class Add:
+def __init__ (self, num1, num2):
+  self.num1 = num1
+  self.num2 = num2
+  return ((self.num1+self.num2) % 26)
+
+class Mult:
+def __init__ (self, num1, num2):
+  self.num1 = num1
+  self.num2 = num2
+  return ((self.num1*self.num2) % 26)
