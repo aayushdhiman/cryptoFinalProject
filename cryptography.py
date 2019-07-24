db06c748c03aacd6922e51bc682813aa9f11213a
PatrickJSli <47364515+PatrickJSli@users.noreply.github.com>
2019-07-24 10:53:44 -0400

    Added IC Calc

--- cryptography.py
+++ cryptography.py
@@ -1,1 +1,12 @@
-
+def calcIC(text):
+    counts = Counter(text)
+    num = 0.0
+    den = 0.0
+    for val in counts.values():
+        i = val
+        num += i * (i - 1)
+        den += i
+    if (den == 0.0):
+        return 0.0
+    else:
+        return num / ( den * (den - 1))
