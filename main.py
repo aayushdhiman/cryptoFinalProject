cd6a6a96e6a6f24d47ad5fcc2666a889646caf11
Knakrack3 <42646291+Knakrack3@users.noreply.github.com>
2019-07-25 09:39:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def mult (num1, num2, mod=26):
 
 def power (num1, num2, mod=26):
   a = num2 % phi(mod)
-  return (num1 ** a) % 26
+  return (num1 ** a) % mod
 def phi (num):
-  list2 = factor(num, list1)
+  list2 = prime_factors(num, list1)
   prod = product(list2)
   s = set(list2)
   for a in s
@@ -21,12 +21,18 @@ def gcd(num1, num2):
     return(num2)
   return(gcd(num2 % num1, num1))
  
-def factor(num1, list1):
- for i in range (2, int(num1 ** 0.5))
-   if(gcd(i,num1) !== 1):
-      list1.append(i)
-      factor((num1 / i), list1)    
- return list1
+def prime_factors(num1):
+    i = 2
+    factors = []
+    while i * i <= num1:
+        if num1 % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 def product(list):
     p = 1
